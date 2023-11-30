92ca45ba3f54ac335af444b39b6b0ebbc8e61bd2
Danel Lafuente <dlafuente003@gmail.com>
2023-11-30 18:41:44 +0100

    default icons added from leaflet

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -3,6 +3,18 @@ import { useState, useEffect } from 'react';
 import  './Map.scss';
 // import leaflet css
 import 'leaflet/dist/leaflet.css';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import L from 'leaflet';
+
+// fix leaflet marker icons
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
+
 
 /**
  * 
